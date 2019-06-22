e42fbce129061d4884044e8162ab533f693f8595
Bruno A. Padua <bapadua@outlook.com>
2019-06-22 20:15:25 -0300

    ObjectNotFoundEx.

---
+++ src/main/java/br/com/bapadua/oauth/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package br.com.bapadua.oauth.exceptions;
+
+import java.io.Serializable;
+
+public class ObjectNotFoundException extends RuntimeException implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
