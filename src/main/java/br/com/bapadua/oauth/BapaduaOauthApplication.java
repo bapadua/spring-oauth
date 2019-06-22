dd41693e1d8f355b016c016bedc56b3e80f35e8b
Bruno A. Padua <bapadua@outlook.com>
2019-06-22 03:23:55 -0300

    Projeto criado

---
+++ src/main/java/br/com/bapadua/oauth/BapaduaOauthApplication.java
@@ -0,0 +1,13 @@
+package br.com.bapadua.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BapaduaOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BapaduaOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bapadua/oauth/BapaduaOauthApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.bapadua.oauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BapaduaOauthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
